7f08ed5e3398f712587b258edd297e863cf6471b
Devin-Dellamano <devin_dellamano@yahoo.com>
2019-08-11 18:59:28 -0500

    Empty Project with a Sandbox for testing.

---
+++ 3D_Rush/Assets/SandboxAssets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+	//[SerializeField] Transform target;
+	//public float speed = 6f;
+	//Vector3 targetPos;
+
+	public List<LayerMask> whatCanBeClickedOn;
+	private NavMeshAgent myAgent;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		myAgent = this.GetComponent<NavMeshAgent>();
+		//targetPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit, 100, whatCanBeClickedOn[0]) ||
+				Physics.Raycast(ray, out hit, 100, whatCanBeClickedOn[1]))
+			{
+				myAgent.SetDestination(hit.point);
+			}
+		}
+		//{
+		//	RaycastHit hit;
+		//	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//	if (Physics.Raycast(ray, out hit))
+		//	{
+		//		if (hit.collider.name == "BreakableWall" || hit.collider.name == "Door")
+		//		{
+		//			targetPos = hit.point;
+		//			targetPos.y = transform.position.y; 
+		//		}
+		//	}
+		//}
+		//if(transform.position != targetPos)
+		//{
+		//	transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+		//}
+	}
+}
